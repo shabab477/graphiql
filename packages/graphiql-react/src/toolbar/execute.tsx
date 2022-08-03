5590012c893f770a95832ff437f1c2c87a453ae8
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-08-03 19:32:37 +0200

    better tooltips

--- packages/graphiql-react/src/toolbar/execute.tsx
+++ packages/graphiql-react/src/toolbar/execute.tsx
@@ -1,7 +1,7 @@
 import { useEditorContext } from '../editor';
 import { useExecutionContext } from '../execution';
 import { PlayIcon, StopIcon } from '../icons';
-import { Menu } from '../ui';
+import { Menu, Tooltip } from '../ui';
 
 import './execute.css';
 
@@ -19,16 +19,20 @@ export function ExecuteButton() {
   const operations = queryEditor?.operations || [];
   const hasOptions = operations.length > 1;
 
+  const label = 'Execute Query (Ctrl-Enter)';
   const buttonProps = {
     type: 'button' as const,
     className: 'graphiql-execute-button',
-    title: 'Execute Query (Ctrl-Enter)',
     children: isRunning ? <StopIcon /> : <PlayIcon />,
+    'aria-label': label,
   };
 
   return hasOptions ? (
     <Menu>
-      <Menu.Button {...buttonProps} />
+      <Tooltip label="Execute Query (Ctrl-Enter)">
+        <Menu.Button {...buttonProps} />
+      </Tooltip>
+
       <Menu.List>
         {operations.map((operation, i) => {
           const opName = operation.name
